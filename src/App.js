99062c4841d2a930d4a8d29fef150e343adf2d04
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-09-19 00:58:15 +0530

    UI Created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import Categories from "./Categories";
-import "./App.css";
+import "./styles.css";
 
 function App() {
+  var cityCategories = {
+    pune: ["shaniwar wada", "Dagdushet Ganpati", "Icskon Temple"],
+    mumbai: ["Marine Drive", "Rani Baug", "Taraporevala Aquarium"],
+    Amritsar: ["Golden Temple", "Jallianwala Baug", "Partition Museum"],
+  };
+
   return (
     <div className="App">
       <h1 className="heading">
         <img
           alt="logo"
-          height="50px"
-          style={{ position: "absolute", left: "15.5rem", top: "1rem" }}
           src="https://media.istockphoto.com/vectors/happy-travel-case-vector-illustration-vector-id1272521633?k=20&m=1272521633&s=612x612&w=0&h=_Is7jGiouNTpRTewuPKSAQ32Yq13p2B3K30Br0U97_g="
         />{" "}
         Travel Diary{" "}
@@ -19,6 +23,9 @@ function App() {
         <Categories City="Pune" />
         <Categories City="Mumbai" />
         <Categories City="Amritsar" />
+      </div>
+      <div className="Places">
+        <hr size="8" width="90%" color="#519c95" />
       </div>
     </div>
   );
