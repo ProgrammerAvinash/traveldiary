c3cfa5f11c6c0cbc1d863f0a2bdb3c6d2e1a6c58
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-09-26 16:27:35 +0530

    Feat: City Places functionality added

--- src/App.js
+++ src/App.js
@@ -3,21 +3,33 @@ import Categories from "./Categories";
 import "./App.css";
 
 function App() {
-  var [Cities, setCities] = useState("");
+  // var [Cities, setCities] = useState("");
+  var [places, setPlaces] = useState("");
 
   var cityCategories = {
-    pune: ["shaniwar wada", "Dagdushet Ganpati", "Icskon Temple"],
-    mumbai: ["Marine Drive", "Rani Baug", "Taraporevala Aquarium"],
-    Amritsar: ["Golden Temple", "Jallianwala Baug", "Partition Museum"],
+    pune: [" shaniwar wada ", "Dagdushet Ganpati ", " Icskon Temple  "],
+    mumbai: [" Marine Drive  ", "Rani Baug", " Taraporevala Aquarium"],
+    Amritsar: [" Golden Temple ", " Jallianwala Baug ", " Partition Museum"],
   };
 
   var CitiesArray = Object.keys(cityCategories);
+
   var citiesMapArray = CitiesArray.map((item) => {
     return item;
   });
-  function onCitiesClicked() {
-    console.log("helo");
+
+  function onCitiesClicked(item) {
+    var CitiesArray = cityCategories[item];
+
+    var PlaceArray = CitiesArray.map((place) => {
+      return place;
+    });
+    setPlaces(PlaceArray);
   }
+  var placesArray = [];
+  placesArray = places;
+
+  console.log("placesArray", placesArray);
   return (
     <div className="App">
       <h1 className="heading">
@@ -29,14 +41,34 @@ function App() {
       </h1>
       <p>Checkout Tourist Places to Visit in Cities</p>
       <div className="Categories">
-        <Categories onClick={onCitiesClicked} City="Pune" />
-        <Categories City="Mumbai" />
-        <Categories City="Amritsar" />
+        <div>
+          {citiesMapArray.map((item) => {
+            return (
+              <button
+                onClick={() => onCitiesClicked(item)}
+                key={item}
+                style={{ padding: "1rem", margin: "10px", cursor: "pointer" }}
+              >
+                {item}
+              </button>
+            );
+          })}
+        </div>
       </div>
       <div className="Places">
         <hr size="8" width="90%" color="#519c95" />
       </div>
+
+      <ul>
+        {Object.values(places).map((place) => {
+          console.log(place);
+
+          return <li style={{ color: "black" }}>{place}</li>;
+        })}
+      </ul>
     </div>
   );
 }
 export default App;
+
+// https://codesandbox.io/s/travel-diary-2-v96efz?file=/src/App.js
